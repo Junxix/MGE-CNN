704c4439eab8fade5b92753869a30dcf1a66223a
Junxix <jjchen20@sjtu.edu.cn>
2022-12-23 11:43:33 +0800

    first commit

---
+++ pretrain.py
@@ -0,0 +1,94 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import utils
+from models import get_model
+from trainer import *
+from dataset import get_loader
+
+
+def main(opt):
+    # model
+    utils.ensure_dir(opt.checkpoint)
+    # model = nn.DataParallel(Net(opt.num_classes, opt=opt)).cuda()
+    model = get_model(opt)
+    model = nn.DataParallel(model).cuda()
+
+
+
+    if opt.model: utils.load_checkpoint(model, opt.model)
+
+    # dataloader
+    train_loader = get_loader(opt, train=True, shuffle=True)['loader']
+    val_loader   = get_loader(opt, train=False, shuffle=False, batch_size=2)['loader']
+
+    # optimizer
+    extractor_params = model.module.get_params(prefix='extractor')
+    classifier_params = model.module.get_params(prefix='classifier')
+    lr_cls = opt.lr
+    lr_extractor = 0.1 * lr_cls
+    if 'lr_rate' in opt.keys():
+        lr_extractor = opt.lr_rate * lr_cls
+    params = [
+              {'params': classifier_params, 'lr': lr_cls},
+              {'params': extractor_params, 'lr': lr_extractor}
+              ]
+    optimizer = torch.optim.SGD(params, lr=opt.lr, momentum=opt.momentum, weight_decay=opt.weight_decay)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+
+    # criterion
+    entropy_loss = nn.CrossEntropyLoss().cuda()
+    criterion = {'entropy': entropy_loss}
+
+    best_loss = 100.
+    epoch_start = 0
+    if opt.resume:
+        checkpoint_dict = utils.load_checkpoint(model, opt.resume)
+        epoch_start = checkpoint_dict['epoch']
+        print(f'Resuming training process from epoch {epoch_start}...')
+        optimizer.load_state_dict(checkpoint_dict['optimizer'])
+        scheduler.load_state_dict(checkpoint_dict['scheduler'])
+
+    # ------main loop-----
+    for epoch in range(epoch_start, opt.epochs):
+        scheduler.step()
+        lr = optimizer.param_groups[0]['lr']
+        print(f"\nEpoch: [{epoch+1:d} | {opt.epochs:d}] LR: {lr:f}")
+
+        tmp_loss = train(train_loader, model, criterion, optimizer, opt)
+        if (epoch + 1)%10 == 0:
+            _ = validate(val_loader, model, opt)
+
+        # ------save checkpoint------
+        is_best = tmp_loss < best_loss
+        best_loss = min(tmp_loss, best_loss)
+        utils.save_checkpoint(
+            {'epoch': epoch + 1,
+            'state_dict': model.module.state_dict(),
+            'optimizer' : optimizer.state_dict(),
+            'scheduler' : scheduler.state_dict()
+            }, is_best, checkpoint=opt.checkpoint)
+
+        if (epoch + 1)%10==0:
+            utils.save_checkpoint(
+                {'epoch': epoch + 1,
+                 'state_dict': model.module.state_dict(),
+                 'state_dict': model.module.state_dict(),
+                 'optimizer' : optimizer.state_dict(),
+                 'scheduler' : scheduler.state_dict()
+                }, is_best, checkpoint=opt.checkpoint, filename=f"epoch_{epoch+1}.pth")
+
+    print(f'Best training loss: {best_loss}')
+    return 0
+
+
+if __name__ == '__main__':
+    cudnn.benchmark = True
+    opt = utils.get_config()
+    utils.set_seeding(opt.seed)
+    os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_ids
+    main(opt)
---
+++ trainer/train.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import time
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+
+from utils import RunningMean, update_meter, accuracy, cutMix, mixUp
+
+def exclude_gt(logit, target, is_log=False):
+    logit = F.log_softmax(logit, dim=-1) if is_log else F.softmax(logit, dim=-1)
+    mask = torch.ones_like(logit)
+    for i in range(logit.size(0)): mask[i, target[i]] = 0
+
+    return mask*logit
+
+
+def kl_loss(x_pred, x_gt, target):
+    # KL Divergence for branch 1 and branch 2
+    kl_gt = exclude_gt(x_gt, target, is_log=False)
+    kl_pred = exclude_gt(x_pred, target, is_log=True)
+    tmp_loss = F.kl_div(kl_pred, kl_gt, reduction='none')
+    tmp_loss = torch.exp(-tmp_loss).mean()
+    return tmp_loss
+
+
+# -----------train----------------------------------
+def train(train_loader, model, criterion, optimizer, args):
+    model = model.train()
+
+    loss_keys = args.loss_keys
+    acc_keys = args.acc_keys
+    loss_meter = {p: RunningMean() for p in loss_keys}
+    acc_meter = {p: RunningMean() for p in acc_keys}
+
+    time_start = time.time()
+    for idx, (input, target) in enumerate(train_loader):
+
+        input = input.cuda()
+        target = target.cuda()
+
+        r = np.random.rand(1)
+        if args.aug == 'cutmix' and r < args.cutmix_prob: 
+            # lam = np.random.beta(args.beta, args.beta)
+            # rand_index = torch.randperm(input.size()[0]).cuda()
+            # target_a = target
+            # target_b = target[rand_index]
+            # bbx1, bby1, bbx2, bby2 = rand_bbox(input.size(), lam)
+            # input[:, :, bbx1:bbx2, bby1:bby2] = input[rand_index, :, bbx1:bbx2, bby1:bby2]
+
+            # lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (input.size()[-1] * input.size()[-2]))
+            target_a, target_b, lam = cutMix(input, target, args.beta)
+            output_dict = model(input, target)
+            logits = output_dict['logits']
+            loss_values = [criterion['entropy'](logit, target_a) * lam + criterion['entropy'](logit, target_b) * (1.0 - lam) for k, logit in enumerate(logits)]
+        elif args.aug == 'mixup' and r < args.mixup_prob:
+            mixed_input, target_a, target_b, lam = mixUp(input, target, args.beta)
+            output_dict = model(mixed_input, target)
+            logits = output_dict['logits']
+            loss_values = [criterion['entropy'](logit, target_a) * lam + criterion['entropy'](logit, target_b) * (1.0 - lam) for k, logit in enumerate(logits)]
+        else:
+            # compute output
+            output_dict = model(input, target)
+            logits = output_dict['logits']
+
+        # -----------------
+            loss_values = [criterion['entropy'](logit, target) for k, logit in enumerate(logits)]
+
+        # if len(loss_keys) > 1:
+        #     kl_loss_1 = kl_loss(logits[5], logits[2].detach(), target)
+        #     kl_loss_2 = kl_loss(logits[8], logits[5].detach(), target)
+
+        #     loss_values.extend([kl_loss_1, kl_loss_2])
+        #     loss_values.append(sum(loss_values))
+        loss_content = {loss_keys[k]: loss_values[k] for k in range(len(loss_keys))}
+
+        # update acc and loss
+        # acc_values = [accuracy(logit, target, topk=(1,))[0] for logit in logits] 
+        # acc_content = {acc_keys[k]: acc_values[k] for k in range(len(acc_keys))}
+
+        update_meter(loss_meter, loss_content, input.size(0))
+        # update_meter(acc_meter, acc_content, input.size(0))
+
+        tmp_str = ''
+        for k, v in loss_meter.items(): tmp_str = tmp_str + f"{k}:{v.value:.4f} "
+        tmp_str = tmp_str + "\n"
+        # for k, v in acc_meter.items(): tmp_str = tmp_str + f"{k}:{v.value:.1f} "
+
+        optimizer.zero_grad()
+        loss_values[-1].backward()
+        optimizer.step()
+
+    time_eclapse = time.time() - time_start
+    print(tmp_str + f"t:{time_eclapse:.1f}s")
+    return loss_meter[loss_keys[-1]].value
